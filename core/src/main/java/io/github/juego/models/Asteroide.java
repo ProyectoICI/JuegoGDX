3e9ca55c30ccf126c0bdc43f70f95308cb875bb0
Proyecto <proyectoestruc101@gmail.com>
2024-11-24 21:19:15 -0300

    Removido el state FleePlayer

--- core/src/main/java/io/github/juego/models/Asteroide.java
+++ core/src/main/java/io/github/juego/models/Asteroide.java
@@ -1,7 +1,6 @@
 package io.github.juego.models;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
